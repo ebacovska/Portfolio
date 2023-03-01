ebbfe13ae501354e0f554345b46f1ac87ec3b347
Elen Bačovská <elen.bacovska@gmail.com>
2023-03-01 13:47:06 +0100

    Render the titles

---
+++ main.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+
+
+st.set_page_config(layout="wide")
+st.balloons()
+
+colom1, colom2 = st.columns(2)
+
+with colom1:
+    st.image("images/12.png")
+
+with colom2:
+    st.title("Lorem Ipsum")
+    content = """Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Nulla non arcu lacinia neque faucibus fringilla. Vestibulum fermentum
+    tortor id mi. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+    laboris nisi ut aliquip ex ea commodo consequat. Aliquam erat volutpat.
+    Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus,
+    nec bibendum odio risus sit amet ante. Lorem ipsum dolor sit amet, consectetuer 
+    adipiscing elit. Aliquam in lorem sit amet leo accumsan lacinia. Fusce aliquam 
+    vestibulum ipsum."""
+    st.info(content)
+
+content2 = """Lorem ipsum dolor sit amet, consectetuer adipiscing elit."""
+
+st.write(content2)
+
+
+colom3, colom4 = st.columns(2)
+
+dataframe = pd.read_csv("data.csv", sep = ";")
+for index, row, in dataframe.iterrows():
+    if index % 2 == 0: 
+        with colom3:
+            st.header(row["title"])
+    else:
+        with colom4:
+            st.header(row["title"])
+# with colom3:
+#     for index, row, in dataframe.iterrows():
+#         st.header(row["title"])
+        
+# with colom4:
+#     for index, row, in dataframe.iterrows():
+#         st.header(row["title"])
+        
