8daa13514a13f8c9861706d6ccc058a014ec03f6
Elen Bačovská <elen.bacovska@gmail.com>
2023-03-21 12:03:16 +0100

    Send email from website

--- pages/Contakt_me.py
+++ pages/Contakt_me.py
@@ -1,11 +1,21 @@
+from send_email import send_email
 import streamlit as st
-import pandas as pd
 
 st.header("Contact me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email adress")
-    user_text = st.text_area("Your message")
+    raw_user_text = st.text_area("Your message")
+    user_text = f"""\
+Subject: New email from {user_email}.
+
+{raw_user_text}  
+"""
+
+     
+
     button = st.form_submit_button("Send")
+    print(button)
     if button:
-        print("hi")
+        send_email(user_text)
+        st.info("Your email was sent succeessfully!")
--- send_email.py
+++ send_email.py
@@ -4,24 +4,20 @@ import smtplib
 import ssl
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
-host = "smtp.gmail.com"
-port = 465
 
-usermane = "elen.bacovska@gmail.com"
-password = os.environ.get("PASSWORD_FOR_EMAIL")
-message = """\
-Subject: Hi
-Hi!
-Haw are you?
-Bye.
-"""
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "elen.bacovska@gmail.com"
-context = ssl.create_default_context()
+    usermane = "elen.bacovska@gmail.com"
+    password = os.environ.get("PASSWORD_FOR_EMAIL")
+    
+    receiver = "elen.bacovska@gmail.com"
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(usermane, password)
-    server.sendmail(usermane, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(usermane, password)
+        server.sendmail(usermane, receiver, message)
+
